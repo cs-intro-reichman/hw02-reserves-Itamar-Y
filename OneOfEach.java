0c63182458683b46e8d3da1c705375324a65f6b7
Itamar-Y <itamar.yaron@post.runi.ac.il>
2024-02-05 23:35:40 +0200

    Fifth Commit - Hw02

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,45 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+
+		double child1 = Math.random();
+		double child2;
+		int boys = 0;
+		int girls = 0;
+		String children = "";
+
+		if (child1 > 0.5) {
+			boys += 1;
+			children += "b ";
+		} else {
+			girls += 1;
+			children += "g ";
+		}
+		
+		do {
+			// System.out.println(children);
+			child2 = child1;
+			child1 = Math.random();
+			if (child1 > 0.5) {
+				boys += 1;
+				children += "b ";
+			} else {
+				girls += 1;
+				children += "g ";
+			}
+		} while (boys == 0 || girls == 0);
+
+
+
+
+		
+
+	int children_amount = boys + girls;
+	System.out.println(children);
+	// System.out.println(children_amount);
+
+
+ System.out.println("You made it... and you now have " + children_amount + " children.");
+	
 	}
 }
