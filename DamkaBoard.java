2eefe9f5bdf83014ff2aa47377dcc7fe78b863bf
Itamar-Y <itamar.yaron@post.runi.ac.il>
2024-02-05 22:03:15 +0200

    Fourth Commit - HW02

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,17 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int size = Integer.parseInt(args[0]);
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (i % 2 == 0) {
+					System.out.print("* ");
+				} else {
+					System.out.print(" *");
+				}
+			} 
+			System.out.println();
+		}
 	}
 }
